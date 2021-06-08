d7233ecfd670fc97aa38917696684c749b785904
MOHAMAD MIRZA <19025376@myrp.edu.sg>
2021-06-08 11:45:41 +0800

    Initial commit

---
+++ app/src/main/java/com/myapplicationdev/android/democooldrawer/AnniversaryFragment.java
@@ -0,0 +1,24 @@
+package com.myapplicationdev.android.democooldrawer;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AnniversaryFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.anniversaryfragment, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/myapplicationdev/android/democooldrawer/VaccinationFragment.java
@@ -0,0 +1,23 @@
+package com.myapplicationdev.android.democooldrawer;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class VaccinationFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.vaccinationfragment, container, false);
+        return view;
+    }
+}
